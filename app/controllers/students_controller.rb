7cddf60d93a1e1cd5ec7965d24a51d11908c4c78
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-09-24 00:27:43 +0300

    Finished lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,14 @@
 class StudentsController < ApplicationController
 
   def index
+   
     students = Student.all
     render json: students
+  end
+  def show
+    #byebug
+    student = Student.find_by(id:params[:id])
+    render json: student
   end
 
 end
